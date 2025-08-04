73db609683894a002369fafa4d51ca996028e3f2
Angellucy460 <angelliona38@gmail.com>
2025-08-04 16:12:40 +0300

    pyhton changed

--- Intro to Python/data_type.py
+++ Intro to Python/data_type.py
@@ -1,8 +1,29 @@
 # snake_case
 # identication
 
-x=10
-pi=3.142
-myName="Angel"
-myName1='Angel'
+x=10 #Integer 
+print(x)
+pi=3.142  #Floating
+print(pi)
+myName="Angel" #String
+print(myName)
+myName1='Angel' #String
+print(myName1)
+is_happy=True # python True/False js true/false
+print(is_happy)
+no_value=None # None is the equivalent to Null in js
+print(no_value)
+
+#Advanced Datatypes Bracket notation
+my_list=[10,10,"string",True,None,123,[]] # List<Array> 
+print(my_list)
+print(my_list[5])
+my_set={1,2,3,3,"Hello","Hello"} #Set(unique stuff only) and Array
+print(my_set)
+my_tuple=(1,3,4,5,"Apple") #tuple my_tuple[4]=Apple
+print(my_tuple)
+my_obj={"key":"value","age":23} #Dictionary Key<->value my_obj["key"]
+print(my_obj)
+
+## Print ALL Value: python print() >---< js<console.log> 
 
