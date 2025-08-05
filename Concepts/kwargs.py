ced68d438fe71465597b1f64a1f952a4a09c695e
Angellucy460 <angelliona38@gmail.com>
2025-08-05 10:57:09 +0300

    kwargs

---
+++ Concepts/kwargs.py
@@ -0,0 +1,15 @@
+## *kwargs -> Key word arguments
+## Dictionary{key:value}
+
+# 3 min :try mixing up
+def greet(name,nationality):
+    print("Name is",name)
+    print("From ",nationality)
+
+#KWARGS PROFILE
+def employee(**kwargs):
+    print(kwargs)
+
+#{key:value}
+employee(name="Samson",age=20,degree="Engineering")
+employee(name="Samson",country="Kenya",degree="Engineering")
