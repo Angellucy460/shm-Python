3c53b78a9f88af047c7e016f9ca2ec31ebafcf12
Angellucy460 <angelliona38@gmail.com>
2025-08-07 09:58:44 +0300

    strings

---
+++ Classes/intro.py
@@ -0,0 +1,19 @@
+#class Name:First letter Cap<> 
+class Student:
+     name="Angel"
+     email="angelliona38@gmail.com"
+     phone="254790781575"
+
+#student 1
+student1=Student() #Object datatype: dot notation
+student2=Student()
+
+print("student 1")
+print("name",student1.name)
+print("email",student1.email)
+print("phone",student1.phone)
+
+print("student 2")
+print("name",student2.name)
+print("email",student2.email)
+print("phone",student2.phone)
